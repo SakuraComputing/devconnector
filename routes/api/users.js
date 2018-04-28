865847c84f4d63f7a894bc1182ed01139218790d
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-28 17:08:02 +0100

    Add Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,29 @@ router.post('/register', (req, res) => {
        })
 });
 
+// @route GET api/users/login
+// @desc Login user / Returning JWT Token
+// @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email})
+        .then(user => {
+            if(!user) {
+                return res.status(404).json({ email: 'User not found'});
+            }
+
+            // Check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({ message: 'Success'})
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect'})
+                    }
+                })
+        })
+});
+
 module.exports = router;
