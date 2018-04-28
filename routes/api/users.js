85789c193b282313909522cae497e316cfef2624
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-28 18:15:53 +0100

    User validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,9 @@ const jwt = require ('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // @route GET api/users/test
 // @desc Tests users route
 // @access Public
@@ -18,10 +21,19 @@ router.get('/test', (req, res) => res.json({msg: "Users works"
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
-   User.findOne({ email: req.body.email })
+
+   const { errors, isValid } = validateRegisterInput(req.body);
+
+   // Check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email })
        .then(user => {
            if(user) {
-               return res.status(400).json({ email: 'Email already exists'});
+               errors.email = 'Email already exists';
+               return res.status(400).json(errors);
            } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200',
