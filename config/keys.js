29e0e88bc51ad250d1c440025d8e39355c2004dd
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-28 16:00:46 +0100

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://Ewan:Miyazaki1@ds161939.mlab.com:61939/devconnector'
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Db Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+app.get('/', (req,res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
