2b2c116b948bac4cb4cdc88a3bfce509fa8475a2
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-28 17:47:39 +0100

    Add jwt check

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require ('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -89,5 +90,16 @@ router.post('/login', (req, res) => {
                 })
         })
 });
+
+// @route GET api/users/current
+// @desc Return current user
+// @access Private
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req,res) => {
+        res.json(req.user);
+    }
+);
 
 module.exports = router;
