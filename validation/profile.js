8cb44bcddd6bb17fa524ac56896e951ee661cb3f
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-29 23:52:01 +0100

    Education validation

--- validation/profile.js
+++ validation/profile.js
@@ -8,8 +8,6 @@ module.exports = function validateProfileInput(data) {
     data.status = !isEmpty(data.status) ? data.status : '';
     data.skills = !isEmpty(data.skills) ? data.skills : '';
 
-    console.log("Profile", data.handle);
-
     if(!Validator.isLength(data.handle, { min: 2, max: 40 })) {
         errors.handle = 'Handle needs to be between 2 and 40 characters'
     }
