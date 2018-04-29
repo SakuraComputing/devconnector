736aa9fc4b883484ce335ca2ce89f42038cc8d92
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-29 16:42:48 +0100

    Add Profile Schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -64,13 +65,21 @@ router.post('/register', (req, res) => {
 // @desc Login user / Returning JWT Token
 // @access Public
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+    // Check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
     User.findOne({ email})
         .then(user => {
             if(!user) {
-                return res.status(404).json({ email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             // Check password
@@ -97,7 +106,8 @@ router.post('/login', (req, res) => {
                             }
                         );
                     } else {
-                        return res.status(400).json({ password: 'Password incorrect'})
+                        errors.password = 'Password Incorrect';
+                        return res.status(400).json(errors)
                     }
                 })
         })
