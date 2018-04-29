ddf2f907a2da61af7f750071df458d0cc810ded6
Ewan Bauld <ewan.bauld@gmail.com>
2018-04-29 17:02:03 +0100

    Add Profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile model
+const Profile = require('../../models/Profile');
+
+//Load User Profile
+const User = require ('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
 // @access Public
-router.get('/test', (req, res) => res.json({msg: "Profile works"
-}));
+router.get('/test', (req, res) => res.json({msg: "Profile works"}));
+
+// @route GET api/profile
+// @desc Get current users profile
+// @access Public
+router.get('/', passport.authenticate('jwt', { session: false}), (req,res) => {
+    const errors = {};
+   Profile.findOne({ user: req.user.id })
+       .then(profile => {
+           if(!profile) {
+               errors.norprofile = 'There is no profile for this user';
+               return res.status(404).json(errors)
+           }
+           res.json(profile);
+       })
+       .catch(err => res.status(404).json(err));
+});
+
+
 
 module.exports = router;
