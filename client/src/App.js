4664a6e38ec8506c017c11b87c7ab4b2115bb0ce
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-01 01:40:19 +0100

    Add Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Navbar />
-          <Landing/>
-          <Footer />
-      </div>
+        <Router>
+          <div className="App">
+              <Navbar />
+              <Route exact path="/" component={Landing} />
+              <div className="container">
+                  <Route exact path="/register" component={Register}></Route>
+                  <Route exact path="/login" component={Login}></Route>
+              </div>
+              <Footer />
+          </div>
+        </Router>
     );
   }
 }
