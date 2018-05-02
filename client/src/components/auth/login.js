f55130712255a12519e8210d5de2ed98f626f8a3
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-02 17:07:38 +0100

    Add login with jwt

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from "../../actions/authActions";
 
 class Login extends React.Component {
 
@@ -11,20 +15,33 @@ class Login extends React.Component {
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+
+        if(nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value })
     };
 
     onSubmit = (e) => {
         e.preventDefault();
-        const user = {
+        const userData = {
             email: this.state.email,
             password: this.state.password
         };
-        console.log(user);
+        this.props.loginUser(userData);
     };
 
     render() {
+
+        const { errors } = this.state;
+
         return(
             <div>
                 <div className="login">
@@ -37,22 +54,28 @@ class Login extends React.Component {
                                     <div className="form-group">
                                         <input
                                             type="email"
-                                            className="form-control form-control-lg"
+                                            className={classnames('form-control from-control-lg', {
+                                                'is-invalid': errors.email
+                                            })}
                                             placeholder="Email Address"
                                             name="email"
                                             value={this.state.email}
                                             onChange={this.onChange}
                                         />
+                                        {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                                     </div>
                                     <div className="form-group">
                                         <input
                                             type="password"
-                                            className="form-control form-control-lg"
+                                            className={classnames('form-control from-control-lg', {
+                                                'is-invalid': errors.password
+                                            })}
                                             placeholder="Password"
                                             name="password"
                                             value={this.state.password}
                                             onChange={this.onChange}
                                         />
+                                        {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                                     </div>
                                     <input
                                         type="submit"
@@ -66,4 +89,16 @@ class Login extends React.Component {
         )
     }
 }
-export default Login;
+
+Login.propTypes = {
+     loginUser: PropTypes.func.isRequired,
+        auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
