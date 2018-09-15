431fc17b3a72d53c98564b7780597d16a17c0c42
Ewan Bauld <ewan.bauld@gmail.com>
2018-09-15 12:33:09 +0100

    Fix production issue

--- client/src/store.js
+++ client/src/store.js
@@ -4,14 +4,25 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
+let setUp;
+
 const middleware = [thunk];
+
+if(process.env.NODE_ENV === 'production') {
+    setUp = compose(applyMiddleware(...middleware)) 
+} else {
+    setUp = compose(applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+}
+
+console.log(process.env.NODE_ENV);
+
 
 const store = createStore(
     rootReducer,
     initialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    setUp
 );
 
 export default store;
